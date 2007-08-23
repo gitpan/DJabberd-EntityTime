aa7348578f0423ef4556cbd6ac03176a3fe2ee3e
Edward Rudd <urkle@cpan.org>
2007-08-23 17:16:29 +0000

    Import of URKLE/DJabberd-EntityTime-0.03 from CPAN.
    gitpan-cpan-distribution: DJabberd-EntityTime
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         URKLE/DJabberd-EntityTime-0.03.tar.gz
    gitpan-cpan-author:       URKLE
    gitpan-cpan-maturity:     released

--- lib/DJabberd/Plugin/EntityTime.pm
+++ lib/DJabberd/Plugin/EntityTime.pm
@@ -10,15 +10,15 @@ our $logger = DJabberd::Log->get_logger();
 
 =head1 NAME
 
-DJabberd::Plugin::EntityTime - Implements XEP-0090 and XEP-0202
+DJabberd::EntityTime - Implements XEP-0090 and XEP-0202
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
